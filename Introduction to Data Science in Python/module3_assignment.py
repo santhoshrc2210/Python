06e2f27fc9d520320f5b8117daf48025ec968181
Santhosh Chavalla <62931371+santhoshrc2210@users.noreply.github.com>
2026-05-01 15:51:59 +0530

    Implement data analysis functions for energy and GDP
    Updated the module to include functions for data analysis on energy indicators and GDP.

--- Introduction to Data Science in Python/module3_assignment.py
+++ Introduction to Data Science in Python/module3_assignment.py
@@ -5,7 +5,7 @@ import re
 # Filter all warnings. If you would like to see the warnings, please comment the two lines below.
 import warnings
 warnings.filterwarnings('ignore')
-
+"""
 Q.1
 Load the energy data from the file assets/Energy Indicators.xls, which is a list of indicators of energy supply and renewable electricity production from the United Nations for the year 2013, and should be put into a DataFrame with the variable name of Energy.
 
@@ -35,7 +35,7 @@ The index of this DataFrame should be the name of the country, and the columns s
 
 This function should return a DataFrame with 20 columns and 15 entries, and the rows of the DataFrame should be sorted by "Rank".
 
-
+"""
 
 def answer_one():
     # YOUR CODE HERE
@@ -175,10 +175,10 @@ def answer_one():
     raise NotImplementedError()
 #print(answer_one())
 
-
+"""
 Q.2
 The previous question joined three datasets then reduced this to just the top 15 entries. When you joined the datasets, but before you reduced this to the top 15 items, how many entries did you lose?
-
+"""
 def answer_two():
     energy_ind='assets/Energy Indicators.xls'
     ENERGY=pd.read_excel(energy_ind)
@@ -298,13 +298,13 @@ def answer_two():
     return missing_rows
 
 #print(answer_two())
-
+"""
 q.3
 
 What are the top 15 countries for average GDP over the last 10 years?
 
 This function should return a Series named avgGDP with 15 countries and their average GDP sorted in descending order.
-
+"""
 def answer_three():
     # YOUR CODE HERE
      #read csv file and skip header
@@ -323,13 +323,13 @@ def answer_three():
     raise NotImplementedError()
     
 #print(answer_three())
-
+"""
 q.4
 
 By how much had the GDP changed over the 10 year span for the country with the 6th largest average GDP?
 
 This function should return a single number.
-
+"""
 def answer_four():
     # YOUR CODE HERE
       #read csv file and skip header
@@ -343,13 +343,13 @@ def answer_four():
     return uk_gdp_15-uk_gdp_06
     raise NotImplementedError()
 #print(answer_four())
-
+"""
 q.5
 
 What is the mean energy supply per capita?
 
 This function should return a single number.
-
+"""
 def answer_five():
     # YOUR CODE HERE
     ENERGY=answer_one()
@@ -363,13 +363,13 @@ def answer_five():
 
 #print(answer_five())    
 
-
+"""
 q.6
 
 What country has the maximum % Renewable and what is the percentage?
 
 This function should return a tuple with the name of the country and the percentage.
-
+"""
 
 def answer_six():
     # YOUR CODE HERE
@@ -387,13 +387,13 @@ def answer_six():
     raise NotImplementedError()
     
 #print(answer_six())
-
+"""
 q.7
 
 Create a new column that is the ratio of Self-Citations to Total Citations. What is the maximum value for this new column, and what country has the highest ratio?
 
 This function should return a tuple with the name of the country and the ratio.
-
+"""
 def answer_seven():
     # YOUR CODE HERE
     df=answer_one()
@@ -409,13 +409,13 @@ def answer_seven():
     return ans 
     raise NotImplementedError()
 #print(answer_seven())
-
+"""
 q.8
 
 Create a column that estimates the population using Energy Supply and Energy Supply per capita. What is the third most populous country according to this estimate?
 
 This function should return the name of the country
-
+"""
 def answer_eight():
     # YOUR CODE HERE
     df=answer_one()
@@ -432,7 +432,7 @@ def answer_eight():
     return ctry
     raise NotImplementedError()
 #print(answer_eight())
-
+"""
 q.9
 
 Create a column that estimates the number of citable documents per person. What is the correlation between the number of citable documents per capita and the energy supply per capita? Use the .corr() method, (Pearson's correlation).
@@ -440,7 +440,7 @@ Create a column that estimates the number of citable documents per person. What 
 This function should return a single number.
 
 (Optional: Use the built-in function plot9() to visualize the relationship between Energy Supply per Capita vs. Citable docs per Capita)
-
+"""
 def answer_nine():
     # YOUR CODE HERE
     df=answer_one()
@@ -458,13 +458,13 @@ def answer_nine():
 #print(answer_nine())    
 
 
-
+"""
 q.10
 
 Create a new column with a 1 if the country's % Renewable value is at or above the median for all countries in the top 15, and a 0 if the country's % Renewable value is below the median.
 
 This function should return a series named HighRenew whose index is the country name sorted in ascending order of rank.
-
+"""
 def answer_ten():
     # YOUR CODE HERE
     df=answer_one()
@@ -481,7 +481,7 @@ def answer_ten():
     
 #print(answer_ten())
 
-
+"""
 q.11
 
 Use the following dictionary to group the Countries by Continent, then create a DataFrame that displays the sample size (the number of countries in each continent bin), and the sum, mean, and std deviation for the estimated population of each country.
@@ -502,7 +502,7 @@ ContinentDict  = {'China':'Asia',
                   'Australia':'Australia', 
                   'Brazil':'South America'}
 This function should return a DataFrame with index named Continent ['Asia', 'Australia', 'Europe', 'North America', 'South America'] and columns ['size', 'sum', 'mean', 'std']
-
+"""
 def answer_eleven():
     # YOUR CODE HERE
     df=answer_one()
@@ -559,13 +559,13 @@ def answer_eleven():
 
 #print(answer_eleven())
 
-
+"""
 q.12
 
 Cut % Renewable into 5 bins. Group Top15 by the Continent, as well as these new % Renewable bins. How many countries are in each of these groups?
 
 This function should return a Series with a MultiIndex of Continent, then the bins for % Renewable. Do not include groups with no countries.
-
+"""
 def answer_twelve():
     # YOUR CODE HERE
     df=answer_one()
@@ -613,7 +613,7 @@ def answer_twelve():
     raise NotImplementedError()
 
 #print(answer_twelve())
-
+"""
 q.13
 
 Convert the Population Estimate series to a string with thousands separator (using commas). Use all significant digits (do not round the results).
@@ -621,7 +621,7 @@ Convert the Population Estimate series to a string with thousands separator (usi
 e.g. 12345678.90 -> 12,345,678.90
 
 This function should return a series PopEst whose index is the country name and whose values are the population estimate string
-
+"""
 def answer_thirteen():
     # YOUR CODE HERE
     df=answer_one()
